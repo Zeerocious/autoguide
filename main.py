55995be47924ad87ff6b58343826c4f4cedfa02d
Zuhair Taleb <zuhair.taleb@autodesk.com>
2025-03-20 14:05:02 -0700

    implementing websocket in main

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 from azure.openai import Convo
+import asyncio
+from websockets.asyncio.server import serve
 
-chat = Convo()
-print("Hello, what can I help you with today?")
-user_message = input()
 
-while user_message != "exit":
-    print(chat.send_message(user_message))
-    user_message = input()
+async def handler(websocket):
+    decider = Convo(max_tokens=10, prompt="decider_prompt.txt")
+    chat = Convo(max_tokens=200, prompt="chat_prompt.txt")
+    # recommender = Convo(max_tokens=300, prompt="recommend_prompt.txt")
+
+    while True:
+        user_message = await websocket.recv()
+        print(user_message)
+
+        # Returns 0 if the user is asking for a recommendation
+        # Returns 1 if the user is asking a general question
+        recommend_flag = decider.send_message(user_message)
+
+        if recommend_flag == 0:
+            # Classification model + Recommender goes here
+            # predict = classification_model.predict(user_message)
+            # Logic here to evaluate prediction and send message
+            continue
+        else:
+            response = chat.send_message(user_message)
+        await websocket.send(response)
+
+
+async def main():
+    async with serve(handler, "", 8001) as server:
+        await server.serve_forever()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
